aad222c5a0fa4dbad79452b5ebde5763b91ebda2
octoberclub <octoberclub@gmail.com>
2018-09-05 23:38:11 +0100

    Add upload button

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
-/*
-sources:
-http://stackoverflow.com/questions/2608022/how-can-i-use-custom-fonts-in-an-html5-canvas-element
+var canvas = document.getElementById('c');
+var ctx = canvas.getContext('2d');
 
-http://stackoverflow.com/questions/2756575/drawing-text-to-canvas-with-font-face-does-not-work-at-the-first-time
-*/
-
-var ctx = document.getElementById('c').getContext('2d');
-
-var myTransBodyPlaceholder = new Image();
-myTransBodyPlaceholder.src = 'rainbow.png';
-myTransBodyPlaceholder.onload = function(){
-  ctx.globalAlpha = 0.4;
-  ctx.drawImage(myTransBodyPlaceholder, 0, 0, myTransBodyPlaceholder.width, myTransBodyPlaceholder.height, 0, 0, 450, 120);
-  ctx.globalAlpha = 1.0;
-  ctx.font      = '60px market_saturdayregular';
-  ctx.fillText  ('#MyTransBody is...', 70, 30);
-};
-
-var profileImage = new Image();
-profileImage.src = 'upload.jpg';
-profileImage.onload = function(){
-  ctx.drawImage(profileImage, 0, 0, profileImage.width,    profileImage.height, 0, 0, 500, 500);
-  ctx.fillStyle = 'white';
-  ctx.textBaseline = 'top';
-};
+canvas.onclick = function(){
+  var preview = document.getElementById('preview');
+  preview.click();
+}
 
 function myFunction(){
   var myTextPlaceholder = new Image();
@@ -36,4 +17,32 @@ function myFunction(){
     var myText = document.getElementById('myText');
     ctx.fillText  (myText.value, 140, 400);
   };
+}
+
+function previewFile(){
+  var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+  var reader  = new FileReader();
+
+  reader.onloadend = function () {
+    var profileImage = new Image();
+    profileImage.src = reader.result;
+    profileImage.onload = function(){
+      ctx.drawImage(profileImage, 0, 0, profileImage.width,    profileImage.height, 0, 0, 500, 500);
+      ctx.fillStyle = 'white';
+      ctx.textBaseline = 'top';
+    };
+    var myTransBodyPlaceholder = new Image();
+    myTransBodyPlaceholder.src = 'rainbow.png';
+    myTransBodyPlaceholder.onload = function(){
+      ctx.globalAlpha = 0.4;
+      ctx.drawImage(myTransBodyPlaceholder, 0, 0, myTransBodyPlaceholder.width, myTransBodyPlaceholder.height, 0, 0, 450, 120);
+      ctx.globalAlpha = 1.0;
+      ctx.font      = '60px market_saturdayregular';
+      ctx.fillText  ('#MyTransBody is...', 70, 30);
+    };
+  }
+
+  if (file) {
+    reader.readAsDataURL(file); //reads the data as a URL
+  }
 }
