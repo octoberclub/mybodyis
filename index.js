f0de8814be5a58ed2200998050bdfe3c58d532f5
octoberclub <octoberclub@gmail.com>
2018-09-05 22:47:07 +0100

    Add text input box

--- index.js
+++ index.js
@@ -7,24 +7,33 @@ http://stackoverflow.com/questions/2756575/drawing-text-to-canvas-with-font-face
 
 var ctx = document.getElementById('c').getContext('2d');
 
-var rainbow = new Image();
-rainbow.src = 'upload.jpg';
+var myTransBodyPlaceholder = new Image();
+myTransBodyPlaceholder.src = 'rainbow.png';
+myTransBodyPlaceholder.onload = function(){
+  ctx.globalAlpha = 0.4;
+  ctx.drawImage(myTransBodyPlaceholder, 0, 0, myTransBodyPlaceholder.width, myTransBodyPlaceholder.height, 0, 0, 450, 120);
+  ctx.globalAlpha = 1.0;
+  ctx.font      = '60px market_saturdayregular';
+  ctx.fillText  ('#MyTransBody is...', 70, 30);
+};
 
-// rainbow.onload = function(){
-//   ctx.drawImage(rainbow, 0, 0, rainbow.width, rainbow.height,
-//                   0, 0, 100, 100);
-// };
-
-var kitty = new Image();
-kitty.src = 'upload.jpg';
-kitty.onload = function(){
-  // ctx.drawImage(this, 0,0,this.width, this.height);
-  ctx.drawImage(kitty, 0, 0, kitty.width,    kitty.height,     // source rectangle
-                   0, 0, 370, 500);
-  ctx.font         = '68px market_saturdayregular';
+var profileImage = new Image();
+profileImage.src = 'upload.jpg';
+profileImage.onload = function(){
+  ctx.drawImage(profileImage, 0, 0, profileImage.width,    profileImage.height, 0, 0, 500, 500);
   ctx.fillStyle = 'white';
   ctx.textBaseline = 'top';
-
-  ctx.fillText  ('#MyTransBody is...', 0, 30);
-  ctx.fillText  ('ready to code', 100, 400);
 };
+
+function myFunction(){
+  var myTextPlaceholder = new Image();
+  myTextPlaceholder.src = 'rainbow.png';
+  myTextPlaceholder.onload = function(){
+    ctx.globalAlpha = 0.4;
+    ctx.drawImage(myTextPlaceholder, 0, 0, myTextPlaceholder.width, myTextPlaceholder.height, 80, 370, 340, 100);
+    ctx.globalAlpha = 1.0;
+    ctx.font      = '40px caviardreams_regular';
+    var myText = document.getElementById('myText');
+    ctx.fillText  (myText.value, 140, 400);
+  };
+}
